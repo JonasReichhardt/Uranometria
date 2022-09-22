17b4fbc6205f24e24cd4c161cdd11cc6e0117622
Jonas Reichhardt <32189954+JonasReichhardt@users.noreply.github.com>
2022-09-22 18:23:12 +0200

    Update code.py

--- code.py
+++ code.py
@@ -7,15 +7,19 @@ joystick = Joystick()
 
 select_wpn = [Button(board.GP13,False),Button(board.GP10,False),Button(board.GP11,False),Button(board.GP12,False)]
 
-load_tubes = [Button(board.GP9,False),Button(board.GP6,False),Button(board.GP7,False),Button(board.GP8,False)]
+load_tubes = [Button(board.GP9,False),Button(board.GP6,False),Button(board.GP7,False),Button(board.GP8,False),Button(board.GP20,False)]
 
-unload_tubes = [Button(None,False),Button(None,False),Button(None,False),Button(None,False)]
+unload_tubes = [Button(None,False),Button(None,False),Button(None,False),Button(None,False),Button(None,False)]
 
-fire = [Button(board.GP16,False),Button(board.GP17,False),Button(board.GP15,False),Button(board.GP14,False)]
+fire = [Button(board.GP16,False),Button(board.GP17,False),Button(board.GP15,False),Button(board.GP14,False),Button(board.GP19,False)]
 
 unload = digitalio.DigitalInOut(board.GP18)
 unload.direction = digitalio.Direction.INPUT
 unload.pull = digitalio.Pull.DOWN
+
+safety = digitalio.DigitalInOut(board.GP21)
+safety.direction = digitalio.Direction.INPUT
+safety.pull = digitalio.Pull.DOWN
 
 for wpn in select_wpn:
     joystick.add_input(wpn)
@@ -35,4 +39,6 @@ while True:
         for i in range(len(load_tubes)):
             unload_tubes[i].source_value = False
             load_tubes[i].bypass = False
+    for i in range(len(fire)):
+        fire[i].bypass = not safety.value
     joystick.update()
