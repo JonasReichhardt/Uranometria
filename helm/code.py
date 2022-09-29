b02cddc86d316a6de92ed95abee5d1ca0561a3b0
Jonas Reichhardt <32189954+JonasReichhardt@users.noreply.github.com>
2022-09-29 17:01:34 +0200

    Update code.py

--- helm/code.py
+++ helm/code.py
@@ -10,13 +10,13 @@ exit_warp = Button(board.GP16,False)
 
 dock_button = Button(board.GP17,False)
 
-led_pins = [board.GP10,board.GP11,board.GP12, board.GP13]
+led_pins = [board.GP13,board.GP12,board.GP11, board.GP10, board.GP9]
 leds = []
 
 for pin in led_pins:
     led_pin = digitalio.DigitalInOut(pin)
     led_pin.direction = digitalio.Direction.OUTPUT
-    leds.push(led_pin)
+    leds.append(led_pin)
 
 for btn in warp:
     joystick.add_input(btn)
@@ -24,6 +24,11 @@ joystick.add_input(exit_warp,dock_button)
 
 
 while True:
-    for i in range(len(warp)-1):
-        leds[i].value = warp[i].is_pressed
+    for i in range(len(warp)):
+        if warp[i].is_pressed:
+            for j in range(i+1):
+                leds[j].value = True
+        else:
+            for j in range(i+1):
+                leds[j].value = False
     joystick.update()
